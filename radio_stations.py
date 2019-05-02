a4b32b0f938f1843eeccc94afe28eb0ebff7bb80
Tomoya Yoshioka <tmsick.me@gmail.com>
2019-05-03 07:16:25 +0900

    Update radio_stations.py

--- radio_stations.py
+++ radio_stations.py
@@ -23,3 +23,23 @@ while states_needed:
     final_stations.add(best_station)
 
 print(final_stations)
+
+
+def greedy_set(data):
+    from functools import reduce
+    everything = reduce(lambda acc, s: acc | s, data.values(), set())
+    ans = set()
+    while everything:
+        pivot = ''
+        max_covered = set()
+        for key, items in data.items():
+            covered = everything & items
+            if len(covered) > len(max_covered):
+                max_covered = covered
+                pivot = key
+        everything -= max_covered
+        ans.add(pivot)
+    return ans
+
+
+print(greedy_set(stations))
